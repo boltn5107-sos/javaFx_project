cbbf722b5e9e739154e7219e8f00a8450ce3028c
boltn5107-sos <boltn5107@gmail.com>
2026-03-13 17:04:26 +0000

    les enums

--- src/main/java/com/avec/enums/RoleComite.java
+++ src/main/java/com/avec/enums/RoleComite.java
@@ -1,5 +1,27 @@
 package com.avec.enums;
 
-public class RoleComite {
+public enum RoleComite {
 
+    PRESIDENT("Président"),
+    SECRETAIRE("Secrétaire"),
+    TRESORIER("Trésorier"),
+    COMPTEUR("Compteur"),
+    AUCUN("Aucun rôle");
+
+    private String description;
+    RoleComite( String description )
+    {
+        this.description = description;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    @Override
+    public String toString()
+    {
+        return description;
+    }
 }
