381c35074f900fda7ed99da7abbaedf335f947b1
boltn5107-sos <boltn5107@gmail.com>
2026-03-13 17:00:10 +0000

    les enums

---
+++ src/main/java/com/avec/enums/JourReunion.java
@@ -0,0 +1,31 @@
+package com.avec.enums;
+
+/**
+ * les jours possibles pour les reunions
+ */
+
+public enum JourReunion {
+    LUNDI("Lundi"),
+    MARDI("Mardi"),
+    MERCREDI("Mercredi"),
+    JEUDI("Jeudi"),
+    VENDREDI("Vendredi"),
+    SAMEDI("Samedi"),
+    DIMANCHE("Dimanche");
+
+
+    private final String libelle;
+    JourReunion(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    @Override
+    public String toString() {
+        return libelle;
+    }
+
+}
