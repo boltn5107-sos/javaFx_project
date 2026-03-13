a7fc5aa5b30e09f8e88223cf94a6748537ccb089
Malick Sow <malick1.sow@uadb.edu.sn>
2026-03-13 21:33:10 +0000

    La connexion entre vers la base de données sur DBConnection

--- src/main/java/com/avec/MainApp.java
+++ src/main/java/com/avec/MainApp.java
@@ -1,5 +1,31 @@
 package com.avec;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+public class MainApp extends Application {
+
+	public void start(Stage stage) {
+		
+		Button btn = new Button("start");
+		
+		VBox vb = new VBox();
+		
+		vb.getChildren().add(vb);
+		
+		Scene scene = new Scene(vb,500,500);
+		stage.setScene(scene);
+		
+		
+		stage.show();
+
+	}
+
+	public static void main(String[] args) {
+
+		launch(args);
+	}
 }
--- src/main/java/com/avec/config/DBConnection.java
+++ src/main/java/com/avec/config/DBConnection.java
@@ -1,5 +1,41 @@
 package com.avec.config;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DBConnection {
+	
+	private static final String URL = "jdbc:mysql://localhost/gestion_avec";
+	private static final String USER = "root";
+	private static final String PASSWORD = "";
+	
+	private static Connection connexion = null;
+	
+	public Connection getConnection() {
+		
+		if(connexion == null) {
+			try {
+				
+				connexion = DriverManager.getConnection(URL,USER,PASSWORD);
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return connexion;
+	}
+	
+	public void closeConnection() {
+		
+		if(connexion != null) {
+			
+			try {
+			connexion.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
