01b16690b01125d4fa5a606fd0e9194f1caddf22
boltn5107-sos <boltn5012@gmai.com>
2026-06-07 09:55:01 +0000

    Site Ville de Thiès sous Joomla 5 ...integration des image

---
+++ administrator/logs/joomla_scheduler.php
@@ -0,0 +1,13 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2026-06-07 09:01:25 UTC
+#Software: Joomla! 5.3.0 Stable [ Timu ] 15-April-2025 16:00 GMT
+
+#Fields: date	time	priority	message
+2026-06-07	09:01:25	INFO	Tâche #02 'Nettoyer les données de la session' en cours d'exécution.
+2026-06-07	09:01:25	INFO	Tâche> SessionGC end
+2026-06-07	09:01:27	INFO	La tâche #02 s'est terminée avec succès en 0.00 (net 0.07) secondes.
+2026-06-07	09:01:40	INFO	Tâche #03 'Envoyer un message de mise à jour Joomla' en cours d'exécution.
+2026-06-07	09:02:00	INFO	Tâche> Could not instantiate mail function.
+2026-06-07	09:02:00	INFO	Tâche> UpdateNotification end
+2026-06-07	09:02:00	INFO	La tâche #03 s'est terminée avec succès en 19.96 (net 19.96) secondes.
